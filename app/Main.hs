361440f6fde286fcb0fa0f4210e3813aa89d3dcb
Ekaterina Bushueva <mayflower.studio@gmail.com>
2023-01-11 16:58:41 +0300

    NFT minting & compilation to file

--- app/Main.hs
+++ app/Main.hs
@@ -1,2 +1,18 @@
+module Main (main) where
+
+import System.FilePath ((</>))
+
+import Plutarch (Config (Config, tracingMode), TracingMode (DoTracing))
+import Ply.Plutarch
+
+import MintingPolicy.NFT (nftPolicy)
+
+-- TODO: change the directory path when we know where to put compiled scripts
+-- Compiles all the scripts and put them to files in the "compiled"s folder
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main =
+  writeTypedScript
+    (Config {tracingMode = DoTracing})
+    "NFT Minting Policy"
+    ("compiled" </> "nftPolicy.plutus")
+    nftPolicy
