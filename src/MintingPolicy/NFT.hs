30a3959771e6e9e21c9075cc2a37e3d057451eb2
Ekaterina Bushueva <mayflower.studio@gmail.com>
2023-01-13 14:49:00 +0300

    refactored

--- src/MintingPolicy/NFT.hs
+++ src/MintingPolicy/NFT.hs
@@ -22,23 +22,32 @@ instance DerivePlutusType PNftRedeemer where
 instance PTryFrom PData PNftRedeemer
 
 nftPolicy :: ClosedTerm (PTxOutRef :--> PTokenName :--> PMintingPolicy)
-nftPolicy = plam $ \ref tn rdm' ctx' -> P.do
+nftPolicy = plam $ \ref tn rdm' ctx -> P.do
     (rdm, _) <- ptryFrom @PNftRedeemer rdm'
-    ctx <- pletFields @'["txInfo", "purpose"] ctx'
-    PMinting mintFlds <- pmatch $ getField @"purpose" ctx
-    let ownSym = pfield @"_0" # mintFlds
-    txInfo <- pletFields @'["inputs", "mint"] $ getField @"txInfo" ctx
     pmatch rdm $ \case 
-      PMintNft _ -> popaque $
-        pif 
-          (pany # plam (\x -> pfield @"outRef" # x #== pdata ref) #$ pfromData $ getField @"inputs" txInfo)
-          (pif 
-              (PValue.pvalueOf # getField @"mint" txInfo # ownSym # tn #== 1)
-              (pconstant ())
-              (ptraceError "Wrong NFT mint amount"))
-          (ptraceError "UTxO not consumed")
-      PBurnNft _ -> popaque $ 
-        pif 
-            (PValue.pvalueOf # getField @"mint" txInfo # ownSym # tn #== -1)
-            (pconstant ())
-            (ptraceError "Wrong NFT mint amount")
+      PMintNft _ -> popaque $ unTermCont $ do
+        checkMintingAmount 1 tn ctx
+        checkUTxOSpent ref ctx
+        pure $ pconstant ()
+      PBurnNft _ -> popaque $ unTermCont $ do 
+        checkMintingAmount (-1) tn ctx
+        pure $ pconstant ()
+
+checkMintingAmount :: Term s PInteger -> Term s PTokenName -> Term s PScriptContext -> TermCont s ()
+checkMintingAmount amt tn ctx' = do 
+    ctx <- tcont $ pletFields @'["txInfo", "purpose"] ctx'
+    PMinting mintFlds <- tcont . pmatch $ getField @"purpose" ctx
+    let ownSym = pfield @"_0" # mintFlds
+    txInfo <- tcont $ pletFields @'["mint"] $ getField @"txInfo" ctx
+    pguardC "Wrong NFT mint amount" $
+      PValue.pvalueOf # getField @"mint" txInfo # ownSym # tn #== amt
+
+checkUTxOSpent :: Term s PTxOutRef -> Term s PScriptContext -> TermCont s ()
+checkUTxOSpent ref ctx' = do 
+    ctx <- tcont $ pletFields @'["txInfo"] ctx'
+    txInfo <- tcont $ pletFields @'["inputs"] $ getField @"txInfo" ctx
+    pguardC "UTxO not consumed" $
+      (pany # plam (\x -> pfield @"outRef" # x #== pdata ref) #$ pfromData $ getField @"inputs" txInfo)
+      
+pguardC :: Term s PString -> Term s PBool -> TermCont s ()
+pguardC s cond = tcont $ \f -> pif cond (f ()) $ ptraceError s
