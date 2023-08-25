02ab95ac77cde0f70c120dbf76b0947dcee6bf05
Ekaterina Bushueva <mayflower.studio@gmail.com>
2023-08-25 09:59:48 +0300

    added time functions

--- src/Ext/Plutarch/Extra/Time.hs
+++ src/Ext/Plutarch/Extra/Time.hs
@@ -1,6 +1,7 @@
 module Ext.Plutarch.Extra.Time where
 
 import Plutarch.Api.V1.Time
+import qualified Plutarch.Monadic as P
 import Plutarch.Num ((#*), (#+))
 import Plutarch.Prelude
 
@@ -21,3 +22,23 @@ minutesToPosixDuration = phoistAcyclic $
     let durationMs = minutesToMilliseconds # durationMinutes
         durationPosix = toPosixTime # durationMs
      in addTimes # startedAt # pdata durationPosix
+
+type OrdinalDay = PInteger
+type Epoch = PInteger -- one epoch lasts `epochSize` days
+type DayOfEpoch = PInteger
+
+epochSize :: Term s PInteger
+epochSize = 100
+
+toOrdinalDay :: Term s (PAsData PPOSIXTime :--> OrdinalDay)
+toOrdinalDay = phoistAcyclic $
+  plam $ \time ->
+    pmatch (pfromData time) $ \(PPOSIXTime ms) -> pdiv # ms # (1000 #* 60 #* 60 #* 24)
+
+toEpoch :: Term s (OrdinalDay :--> Epoch)
+toEpoch = phoistAcyclic $
+  plam $ \ordinalDay -> pdiv # ordinalDay # epochSize
+
+toDayOfEpoch :: Term s (OrdinalDay :--> DayOfEpoch)
+toDayOfEpoch = phoistAcyclic $
+  plam $ \ordinalDay -> pmod # ordinalDay # epochSize
